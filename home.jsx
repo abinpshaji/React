9b98b3a773a4946e5fd28040ad5e461e18007407
abinpshaji <139356504+abinpshaji@users.noreply.github.com>
2025-10-16 12:30:37 +0530

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <>
+            <h1 style={{textAlign:"center",textDecoration:"2px underline solid"}}>Home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, nulla laborum dignissimos sit incidunt iste aperiam dolorum quos repellendus repellat optio saepe, autem possimus vero distinctio corporis laboriosam ab quod ducimus quia, aliquid alias? Sequi nobis ducimus quibusdam sunt excepturi voluptate nemo voluptatum, quod enim numquam inventore ex optio commodi! Nulla amet nisi, tempora iste officiis obcaecati harum dignissimos fugiat nam. Tenetur dolor recusandae, vero reprehenderit dicta ab officiis. Tenetur porro, praesentium temporibus maiores cupiditate dolor facere dolore laborum, officiis at, officia perferendis neque. Nulla dolores consequuntur, maiores tempore, itaque soluta eum omnis quidem reprehenderit ipsam ea? Voluptatibus, quia fugit.</p>
+        </>
+    )
+}
+
+export default Home;
